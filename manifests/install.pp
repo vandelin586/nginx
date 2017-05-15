22fcbf128ae7e235144936cf6f8a5eac5baa1ecc
Ryan Vandelinder <ryan@autostructure.io>
2017-05-15 10:32:17 -0500

    stack ers?

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,9 @@
 #Install this silly bugger
 class vandenginx::install{
+  class { 'nginx': }
 
 
-  nginx::resource::server {'nginx':
+  nginx::resource::server {'nginx.attlocal.net':
   ensure  => present,
   www_root => '/opt/html/consul.html',
   require  => File['host'],
