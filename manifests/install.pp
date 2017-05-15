994f84e71f1248a8b44b85cf2ee350d058995300
Ryan Vandelinder <ryan@autostructure.io>
2017-05-15 09:53:15 -0500

    push

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,10 +1,12 @@
 #Install this silly bugger
 
-
+class 'vandenginx::install': {
 class{'nginx':}
 
 
 nginx::resource::vhost {'nginx.attlocal.net':
   www_root => '/opt/html/consul.html',
   require  => File['host'],
+}
+
 }
