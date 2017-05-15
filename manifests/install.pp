b5a3a6fe16d26e88fc05b07a658752c4e0c96c80
Ryan Vandelinder <ryan@autostructure.io>
2017-05-15 11:38:54 -0500

    changes

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,12 +1,33 @@
 #Install this silly bugger
 class vandenginx::install{
-  class { 'nginx': }
+  class { '::nginx': }
 
 
   nginx::resource::server {'nginx.attlocal.net':
-  ensure  => present,
-  www_root => '/opt/html',
+  listen_port => 80,
+  www_root    => '/opt/html',
+  ssl         => false,
 #  require  => File['host'],
   }
 
+  class{'::nginx':
+    manage_repo    => true,
+    package_source => 'nginx-mainline',
+
 }
+
+  nginx::resource::upstream { 'upstream_app':
+    members => [
+      '192.168.40.10:80',
+    ],
+  }
+
+  nginx::resource::server{'nginx.attlocal.net':
+    www_root => '/opt/html/',
+  }
+
+  #nginx::resource::location{'/proxy':
+  #  proxy => 'http://upstream_app/' ,
+  #  server => 'www.myhost.com',
+
+  }
