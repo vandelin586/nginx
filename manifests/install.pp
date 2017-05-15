2139b8bb6243c523aeeb0dd0f2899ddd7cf49ea6
Ryan Vandelinder <ryan@autostructure.io>
2017-05-15 10:27:48 -0500

    nil array

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,7 +2,7 @@
 class vandenginx::install{
 
 
-  nginx::resource::server {'nginx.attlocal.net':
+  nginx::resource::server {'nginx':
   ensure  => present,
   www_root => '/opt/html/consul.html',
   require  => File['host'],
