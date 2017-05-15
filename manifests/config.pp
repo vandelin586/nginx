79d21bd0bf491408b1589e3cd58039497b9a7889
Ryan Vandelinder <ryan@autostructure.io>
2017-05-15 11:27:24 -0500

    class

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,10 +1,11 @@
 #config setup
-
-file { 'puppro':
+class { vandenginx::config {
+  file { 'puppro':
   ensure => present,
   path   => '/opt/html/index.html',
   owner  => 'root',
   group  => 'root',
   mode   => '0644',
   source => 'puppet:///modules/vandenginx/index.html',
+  }
 }
