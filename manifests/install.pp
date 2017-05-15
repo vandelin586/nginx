900118172d02e352920cd704e7d08650c3b579a3
Ryan Vandelinder <ryan@autostructure.io>
2017-05-15 11:46:56 -0500

    reorder

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,19 +1,18 @@
 #Install this silly bugger
 class vandenginx::install{
 
+  class{'::nginx':
+    manage_repo    => true,
+    package_source => 'nginx-mainline',
 
+  }
+  
   nginx::resource::server {'nginx.attlocal.net':
   listen_port => 80,
   www_root    => '/opt/html',
   ssl         => false,
 #  require  => File['host'],
   }
-
-  class{'::nginx':
-    manage_repo    => true,
-    package_source => 'nginx-mainline',
-
-}
 
   nginx::resource::upstream { 'upstream_app':
     members => [
