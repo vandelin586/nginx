e709b15bd046cc151ae5293d3c1832e16011b5d5
Ryan M Vandelinder <ryan@autostructure.io>
2017-06-27 10:33:29 -0500

    comment out upstream

--- manifests/install.pp
+++ manifests/install.pp
@@ -17,7 +17,7 @@ class vandenginx::install{
   nginx::resource::upstream { 'upstream_app':
     members => [
     #  '192.168.40.10:80',
-      '192.168.40.10:8000',
+    #  '192.168.40.10:8000',
     ],
   }
 
